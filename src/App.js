60ce7d0cf824766759f7fe393d5eaeaf1b52f047
Vijay Jha <vjha2323@gmail.com>
2021-05-10 17:13:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react'
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import HomeOutPage from './components/HomeOutPage';
+import { connect } from 'react-redux'
+import HomeInPage from './components/HomeInPage';
+import Login from './components/Login';
+import Product from './components/Product';
 
-function App() {
+
+
+
+
+function App(props) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='ui container'>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+
+            <div>
+              {props.isSignedIn ? <HomeInPage /> : <HomeOutPage />}
+            </div>
+          </Route>
+
+          <Route path='/login'>
+            <Login />
+          </Route>
+
+
+          <Route
+            exact path="/:id"
+            render={(props) => <Product {...props} />}
+          />
+
+        </Switch>
+
+      </BrowserRouter>
+
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    isSignedIn: state.auth.isSignedIn
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import reduxThunk from 'redux-thunk'
 import reportWebVitals from './reportWebVitals';
+import reducers from './reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
